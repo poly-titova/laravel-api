046e2f2b484209880b35decc3830fb7928e95f4c
Poly A. Titova <poly_titova@mail.ru>
2023-09-24 20:07:55 +0300

    Добавили обновление и удаление данных

--- app/Http/Controllers/Api/DeskController.php
+++ app/Http/Controllers/Api/DeskController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\DeskStoreRequest;
 use App\Http\Resources\DeskResource;
 use App\Models\Desk;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class DeskController extends Controller
 {
@@ -15,7 +16,7 @@ class DeskController extends Controller
      */
     public function index()
     {
-        return DeskResource::collection(Desk::with('lists')->get());
+        return DeskResource::collection(Desk::all());
     }
 
     /**
@@ -31,24 +32,28 @@ class DeskController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Desk $desk)
     {
-        return new DeskResource(Desk::with('lists')->findOrFail($id));
+        return new DeskResource($desk);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(DeskStoreRequest $request, Desk $desk)
     {
-        //
+        $desk->update($request->validated());
+
+        return new DeskResource($desk);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Desk $desk)
     {
-        //
+        $desk->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
